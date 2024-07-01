b559fce21a049afaa5aec4b9dfdc642707e98527
FRANCISCOJESUS1980 <jesusgonzalezvergara@hotmail.com>
2024-07-01 21:11:24 +0200

    feat otro arreglo header y poner info autor

--- main.js
+++ main.js
@@ -71,16 +71,29 @@ function displayImages(images) {
   images.forEach((image) => {
     const imgElement = document.createElement('div')
     imgElement.classList.add('image-item')
-    imgElement.innerHTML = `<img src="${image.urls.small}" alt="${image.alt_description}">
-     <div class="user-info">
+    imgElement.innerHTML = `<img src="${image.urls.small}" alt="${
+      image.alt_description
+    }">
+     <div class="user-info"style="background-color: ${getRandomColor()};>
         <a href="${image.user.links.html}" target="_blank">
-          <img src="${image.user.profile_image.small}" alt="${image.user.name}" class="user-profile-pic">
+          <img src="${image.user.profile_image.small}" alt="${
+      image.user.name
+    }" class="user-profile-pic">
           ${image.user.name}
         </a>
       </div>`
     imageContainer.appendChild(imgElement)
   })
 }
+function getRandomColor() {
+  const letters = '0123456789ABCDEF'
+  let color = '#'
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+  return color
+}
+
 function displaySuggestion(message) {
   clearSuggestion()
   const suggestionElement = document.createElement('div')
