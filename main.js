ba7a2adeb81cb78f888909997e06de087911e36d
FRANCISCOJESUS1980 <jesusgonzalezvergara@hotmail.com>
2024-07-01 13:22:55 +0200

    feat otro arreglo header y mas fotos

--- main.js
+++ main.js
@@ -51,3 +51,19 @@ function displayImages(images) {
     imageContainer.appendChild(imgElement)
   })
 }
+function displaySuggestion(message) {
+  clearSuggestion()
+  const suggestionElement = document.createElement('div')
+  suggestionElement.id = 'suggestion'
+  suggestionElement.textContent = message
+  suggestionElement.style.color = 'red'
+  suggestionElement.style.marginTop = '10px'
+  imageContainer.parentElement.insertBefore(suggestionElement, imageContainer)
+}
+
+function clearSuggestion() {
+  const suggestionElement = document.getElementById('suggestion')
+  if (suggestionElement) {
+    suggestionElement.remove()
+  }
+}
